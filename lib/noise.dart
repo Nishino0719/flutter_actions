d7990bbd216cfb9ca464731c78dea481476b80eb
Nishino0719 <nishino0719@icloud.com>
2020-09-16 14:36:12 +0900

    add noise metor

--- lib/noise.dart
+++ lib/noise.dart
@@ -1,15 +1,78 @@
+import 'package:noise_meter/noise_meter.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
-class NoisePage extends StatelessWidget{
+
+class NoisePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar:AppBar (
-      title: new Text('Noiseのページ')
-      ),
-      body: Container(
-        height: double.infinity,
-        color: Colors.red,
+  _NoisePageState createState() => new _NoisePageState();
+
+}
+
+class _NoisePageState extends State<NoisePage>{
+  bool _isRecording = false;
+  StreamSubscription<NoiseReading> _noiseSubscription;
+  NoiseMeter _noiseMeter = new NoiseMeter();
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  void onData(NoiseReading noiseReading) {
+    this.setState(() {
+      if (!this._isRecording) {
+        this._isRecording = true;
+      }
+    });
+    print(noiseReading.toString());
+  }
+
+  void start() async {
+    try {
+      _noiseSubscription = _noiseMeter.noiseStream.listen(onData);
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  void stop() async {
+    try {
+      if (_noiseSubscription != null) {
+        _noiseSubscription.cancel();
+        _noiseSubscription = null;
+      }
+      this.setState(() {
+        this._isRecording = false;
+      });
+    } catch (err) {
+      print('stopRecorder error: $err');
+    }
+  }
+
+  List<Widget> getContent() => <Widget>[
+    Container(
+        margin: EdgeInsets.all(25),
+        child: Column(children: [
+          Container(
+            child: Text(_isRecording ? "Mic: ON" : "Mic: OFF",
+                style: TextStyle(fontSize: 25, color: Colors.blue)),
+            margin: EdgeInsets.only(top: 20),
+          )
+        ])),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: getContent())),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: _isRecording ? Colors.red : Colors.green,
+            onPressed: _isRecording ? stop : start,
+            child: _isRecording ? Icon(Icons.stop) : Icon(Icons.mic)),
       ),
     );
   }
