1e61c01408dad8c5e8e9174835bcbaba6f345025
Nishino0719 <nishino0719@icloud.com>
2020-09-22 22:31:37 +0900

    change the size of the image according to the count

--- lib/bokashi.dart
+++ lib/bokashi.dart
@@ -1,40 +1,10 @@
 import 'dart:ui';
-
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
 
-// class BokashiPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: new ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: new Scaffold(
-//         appBar: AppBar(
-//           title: Text('BackdropFilter example'),
-//         ),
-//         body: new Stack(
-//           children: <Widget>[
-//             Image.network(
-//                 "https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4"),
-//             Positioned.fill(
-//               child: BackdropFilter(
-//                 filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-//                 child: Container(
-//                   color: Colors.black.withOpacity(0),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
+// https://medium.com/flutter-jp/transition-9c57528c84b8
 class BokashiPage extends StatefulWidget {
-  
   @override
   _BokashiPageState createState() => _BokashiPageState();
 }
@@ -43,6 +13,13 @@ class _BokashiPageState extends State<BokashiPage>
     with SingleTickerProviderStateMixin {
   AnimationController _animationController;
   var _isScaledUp = false;
+  static double _accelmaxX = 0;
+  static double _accelmaxY = 0;
+  static double _accelmaxZ = 0;
+  static double _countup_accel = 0;
+  List<double> _accelerometerValues;
+  List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
 
   @override
   void initState() {
@@ -51,17 +28,44 @@ class _BokashiPageState extends State<BokashiPage>
       vsync: this,
       duration: const Duration(milliseconds: 500),
     );
+    _streamSubscriptions
+        .add(accelerometerEvents.listen((AccelerometerEvent event) {
+      if (event.x > _accelmaxX) {
+        _accelmaxX = event.x;
+      } else if (event.y > _accelmaxY) {
+        _accelmaxY = event.y;
+      } else if (event.z > _accelmaxZ) {
+        _accelmaxZ = event.z;
+      }
+
+      if (event.x > 50) {
+        _countup_accel++;
+      }
+      setState(() {
+        _accelerometerValues = <double>[
+          _accelmaxX,
+          _accelmaxY,
+          _accelmaxZ,
+          _countup_accel
+        ];
+      });
+    }));
   }
 
   @override
   void dispose() {
     _animationController.dispose();
     super.dispose();
+    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    final List<String> accelerometer =
+        _accelerometerValues?.map((double v) => v.toStringAsFixed(1))?.toList();
+    return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (_isScaledUp) {
@@ -73,39 +77,53 @@ class _BokashiPageState extends State<BokashiPage>
         },
         child: const Icon(Icons.refresh),
       ),
-      body: Center(child:SizeTransition(
-        axis: Axis.vertical, // default
-        axisAlignment: 0,
-        sizeFactor: _animationController
-            .drive(
-              CurveTween(curve: Curves.fastOutSlowIn),
-            )
-            .drive(
-              Tween<double>(
-                begin: 0.01,
-                end: 1,
-              ),
+      body: Column(
+        children: [
+          Padding(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Accelerometer: $accelerometer'),
+              ],
             ),
-        child:Column(
-          children: [
-            Row(children: <Widget>[
-              Image.network(
-                  "https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4"),
-              Positioned.fill(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                  child: Container(
-                    color: Colors.black.withOpacity(1),
-                  ),
+            padding: const EdgeInsets.all(16.0),
+          ),
+          Center(
+            child: SizeTransition(
+              axis: Axis.vertical, // default
+              axisAlignment: 0,
+              sizeFactor: _animationController
+                  .drive(
+                CurveTween(curve: Curves.fastOutSlowIn),
+              )
+                  .drive(
+                Tween<double>(
+                  begin: 0.01,
+                  end: _countup_accel*0.001,
                 ),
               ),
-            ],
+              child: Column(
+                children: [
+                  Row(
+                    children: <Widget>[
+                      Image.network(
+                          "https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4"),
+                      Positioned.fill(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                          child: Container(
+                            color: Colors.black.withOpacity(1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      ),
+          ),
+        ],
+      )
     );
   }
 }
-
